f266203c7703423e0cae8fd60b09d0d4a4801935
Patrick <patlin1003@skola.goteborg.se>
2022-01-26 16:05:33 +0100

    Rename game class
    Game class renamed and relevant classes refactored

--- desktop/src/se/yrgo/game/desktop/DesktopLauncher.java
+++ desktop/src/se/yrgo/game/desktop/DesktopLauncher.java
@@ -2,13 +2,11 @@ package se.yrgo.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import se.yrgo.game.App;
+import se.yrgo.game.JumpyBirb;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 480;
-		config.height = 800;
-		new LwjglApplication(new App(), config);
+		new LwjglApplication(new JumpyBirb(), config);
 	}
 }
