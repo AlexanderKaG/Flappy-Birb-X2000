97006b29362902dd255446effd3c343a611c81c4
Patrick <patlin1003@skola.goteborg.se>
2022-01-26 11:19:03 +0100

    Initial commit

---
+++ desktop/src/se/yrgo/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package se.yrgo.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import se.yrgo.game.App;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new App(), config);
+	}
+}
