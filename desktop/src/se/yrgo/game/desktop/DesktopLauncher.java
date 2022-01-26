5a62f6c2fe1879a9ad68a783e5bf56c819a2bda2
Patrick <patlin1003@skola.goteborg.se>
2022-01-26 16:14:29 +0100

    Set game window
    Window size and title set.

--- desktop/src/se/yrgo/game/desktop/DesktopLauncher.java
+++ desktop/src/se/yrgo/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import se.yrgo.game.JumpyBirb;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 480;
+		config.height = 800;
+		config.title = "Jumpy Birb";
 		new LwjglApplication(new JumpyBirb(), config);
 	}
 }
