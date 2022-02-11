c820cdd6d94b8a7a841ef8c7dc41cd4a649967bd
Robert <robmoi0112@skola.goteborg.se>
2022-02-11 10:30:30 +0100

    camera

--- core/src/se/yrgo/game/JumpyBirb.java
+++ core/src/se/yrgo/game/JumpyBirb.java
@@ -3,6 +3,7 @@ package se.yrgo.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
@@ -12,8 +13,9 @@ public class JumpyBirb extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private Texture img;
 	private Rectangle birb;
-	private static final float GRAVITY = 0.2f;
+	private static float GRAVITY = 0.2f;
 	private float velocity = 0.0f;
+	private OrthographicCamera camera;
 	
 	@Override
 	public void create () {
@@ -21,12 +23,31 @@ public class JumpyBirb extends ApplicationAdapter {
 		img = new Texture(Gdx.files.internal("Bird.png"));
 
 		birb = new Rectangle();
-		birb.x = 100;
-		birb.y = 400;
+		birb.x = -100;
+		birb.y = 200;
+		birb.width = 64;
+		birb.height = 64;
+
+		camera = new OrthographicCamera(); //skapar en kamera
+		camera.setToOrtho(false);
+		camera.position.set(0, 0 ,0); //camerans position
+
+
 	}
 
 	@Override
+	public void resize(int widht, int height){ //entiteten behåller sin storlek även om man har fullscreen
+		camera.viewportWidth = 400;
+		camera.viewportHeight = 400;
+	}
+
+
+	@Override
 	public void render () {
+
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+
 		ScreenUtils.clear(1, 0, 0, 1);
 
 		batch.begin();
@@ -35,6 +56,15 @@ public class JumpyBirb extends ApplicationAdapter {
 		velocity += GRAVITY;
 		birb.y -= velocity;
 		jump();
+
+		if (birb.y > 200){
+			GRAVITY = 0;
+			velocity = 0;
+		}else if(birb.y < -200 ){
+			GRAVITY = 0;
+			velocity = 0;
+		}
+
 
 		batch.end();
 	}
