9b2d96710ff351d24a2871baaa56ea9dd613a477
Patrick <patlin1003@skola.goteborg.se>
2022-02-18 15:58:44 +0100

    Reset game when Birb hits the ground
    Mouse click added as alternative to initiate gravity to prevent Birb from going upwards
    Comments added

--- core/src/se/yrgo/game/sprites/Birb.java
+++ core/src/se/yrgo/game/sprites/Birb.java
@@ -41,5 +41,8 @@ public class Birb {
     public void setGravity(float gravity) {
         this.gravity = gravity;
     }
-}
 
+    public void setVelocity(float velocity) {
+        this.velocity = velocity;
+    }
+}
