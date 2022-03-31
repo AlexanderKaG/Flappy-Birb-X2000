33091446cc24889bd2735df1fbafc8514745fda2
Robert <robmoi0112@skola.goteborg.se>
2022-03-31 09:40:49 +0200

    new difficulty faces

--- core/src/se/yrgo/game/MainMenuScreen.java
+++ core/src/se/yrgo/game/MainMenuScreen.java
@@ -20,12 +20,12 @@ public class MainMenuScreen implements Screen {
     public MainMenuScreen(JumpyBirbGame game) {
         this.game = game;
         this.startBackground = new Texture(Gdx.files.internal("NewBackground.png"));
-        this.imageEasy = new Texture(Gdx.files.internal("EASY.png"));
-        this.imageMedium = new Texture(Gdx.files.internal("MEDIUM.png"));
-        this.imageHard = new Texture(Gdx.files.internal("HARD.png"));
-        this.positionEasyImage = new Rectangle(50, 250, imageEasy.getWidth(), imageEasy.getHeight());
-        this.positionMediumImage = new Rectangle(300, 250, imageMedium.getWidth(), imageMedium.getHeight());
-        this.positionHardImage = new Rectangle(550, 250, imageHard.getWidth(), imageHard.getHeight());
+        this.imageEasy = new Texture(Gdx.files.internal("Easyy.png"));
+        this.imageMedium = new Texture(Gdx.files.internal("Mediumm.png"));
+        this.imageHard = new Texture(Gdx.files.internal("Hardd.png"));
+        this.positionEasyImage = new Rectangle(80, 290, imageEasy.getWidth(), imageEasy.getHeight()); //x + 30 , y + 20
+        this.positionMediumImage = new Rectangle(330, 290, imageMedium.getWidth(), imageMedium.getHeight());
+        this.positionHardImage = new Rectangle(580, 290, imageHard.getWidth(), imageHard.getHeight());
         this.touchImage = new Vector2();
         backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("man-is-he-mega.mp3"));
         backgroundMusic.setLooping(true);
