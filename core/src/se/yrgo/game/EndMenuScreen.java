f97ecc47cb90f3b1620f2e0c719af65f1a4053c5
AlexanderKaG <alexanderkarlssongoransson@hotmail.com>
2022-04-09 23:41:05 +0200

    Added scoreboard to end screen. Scoreboard keep track of all scores during the current session and also displays the currents session's hiscore.

--- core/src/se/yrgo/game/EndMenuScreen.java
+++ core/src/se/yrgo/game/EndMenuScreen.java
@@ -8,24 +8,42 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class EndMenuScreen implements Screen {
     private final JumpyBirbGame game;
-    private final int endScore;
     private final Texture endBackground;
     private final MainMenuScreen mainMenuScreen;
 
-    public EndMenuScreen(JumpyBirbGame game, int endScore) {
+    public EndMenuScreen(JumpyBirbGame game) {
         this.game = game;
-        this.endScore = endScore;
         this.endBackground = new Texture(Gdx.files.internal("NewBackground.png"));
         this.mainMenuScreen = new MainMenuScreen(game);
+
+        addScoreToScoreboard();
+    }
+
+    private void addScoreToScoreboard() {
+        game.getScoreboard().add(JumpyBirb.getScore());
     }
 
     @Override
     public void render(float delta) {
         ScreenUtils.clear(1, 0, 0, 1);
 
+        int highScore = 0;
+        int scoreYPositionInScoreboard = 650;
         game.getSpritebatch().begin();
         game.getSpritebatch().draw(endBackground, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        game.getFont().draw(game.getSpritebatch(), "Ooops... you died! Score: " + endScore, 300, 700);
+        game.getFont().draw(game.getSpritebatch(), "Ooops... you died! Score: " + JumpyBirb.getScore(), 300, 700);
+        game.getFont().draw(game.getSpritebatch(), "Previous scores:", 300, scoreYPositionInScoreboard);
+
+        for (int score : game.getScoreboard()) {
+            game.getFont().draw(game.getSpritebatch(), Integer.toString(score), 300, scoreYPositionInScoreboard - 20);
+            scoreYPositionInScoreboard -= 20;
+            if (score > highScore) {
+                highScore = score;
+            }
+        }
+
+        game.getFont().draw(game.getSpritebatch(), "High-score: " + highScore, 500, 650);
+
         game.getFont().draw(game.getSpritebatch(), "Press space to restart", 300, 100);
 
         mainMenuScreen.drawDifficultyImages();
@@ -35,6 +53,7 @@ public class EndMenuScreen implements Screen {
         game.getSpritebatch().end();
 
         if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+            JumpyBirb.setScore(0);
             game.setScreen(new JumpyBirb(game));
             dispose();
         }
--- core/src/se/yrgo/game/JumpyBirbGame.java
+++ core/src/se/yrgo/game/JumpyBirbGame.java
@@ -3,16 +3,24 @@ package se.yrgo.game;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
 
 public class JumpyBirbGame extends Game {
     private SpriteBatch spritebatch;
     private BitmapFont font;
 
+    private Array<Integer> scoreboard;
+
     @Override
     public void create() {
         this.spritebatch = new SpriteBatch();
         this.font = new BitmapFont();
+        scoreboard = new Array<>();
         this.setScreen(new MainMenuScreen(this));
+    }
+
+    public Array<Integer> getScoreboard() {
+        return scoreboard;
     }
 
     public SpriteBatch getSpritebatch() {
